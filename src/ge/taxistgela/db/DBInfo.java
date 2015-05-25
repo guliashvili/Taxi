92a480d8d57ea39e81bf936dee98b991456debe3
Alex <alex.r.azizian@gmail.com>
2015-05-25 21:28:13 +0400

    Added stub db package

---
+++ src/ge/taxistgela/db/DBInfo.java
@@ -0,0 +1,12 @@
+package ge.taxistgela.db;
+
+/**
+ * Created by Alex on 5/25/2015.
+ */
+public class DBInfo {
+
+    public static final String MYSQL_USERNAME = "";
+    public static final String MYSQL_PASSWORD = "";
+    public static final String MYSQL_DATABASE_SERVER = "";
+    public static final String MYSQL_DATABASE_NAME = "";
+}
