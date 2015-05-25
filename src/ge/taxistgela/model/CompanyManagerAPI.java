b156428b24c50533ac9544c184589131146cdca4
gio <vinmesmiti@gmail.com>
2015-05-25 21:58:30 +0400

    companyDaoAPI private

--- src/ge/taxistgela/model/CompanyManagerAPI.java
+++ src/ge/taxistgela/model/CompanyManagerAPI.java
@@ -6,7 +6,7 @@ import ge.taxistgela.dao.CompanyDaoAPI;
  * Created by GIO on 5/25/2015.
  */
 public abstract class CompanyManagerAPI {
-     CompanyDaoAPI companyDao;
+     private CompanyDaoAPI companyDao;
      public  CompanyManagerAPI(CompanyDaoAPI companyDao){
          this.companyDao = companyDao;
      }
