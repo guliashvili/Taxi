66af8706a416b9f36aa00cfbc559b7ab01b573b3
gio <vinmesmiti@gmail.com>
2015-05-27 23:47:03 +0400

    updated database.sql

--- src/ge/taxistgela/db/database.sql
+++ src/ge/taxistgela/db/database.sql
@@ -14,7 +14,6 @@ CREATE TABLE Cars (
 CREATE TABLE Companies (
   companyID   INT AUTO_INCREMENT PRIMARY KEY,
   companyCode VARCHAR(9) UNIQUE  NOT NULL,
-  username    VARCHAR(50) UNIQUE NOT NULL,
   email       VARCHAR(50) UNIQUE NOT NULL,
   password    TEXT               NOT NULL,
   companyName VARCHAR(50) UNIQUE NOT NULL,
@@ -23,31 +22,31 @@ CREATE TABLE Companies (
   googleID    TEXT -- TODO trigger davadot uniquesi
 );
 
-CREATE TABLE DriverPreferenes (
-  ID        INT AUTO_INCREMENT PRIMARY KEY,
-  rating    DOUBLE NOT NULL,
-  timeLimit INT    NOT NULL
+CREATE TABLE DriverPreferences(
+  DriverPreferenceID        INT AUTO_INCREMENT PRIMARY KEY,
+  minimumUserRating    DOUBLE NOT NULL,
+  coefficientPer       DOUBLE NOT NULL
 );
 CREATE TABLE Drivers (
   driverID      INT AUTO_INCREMENT PRIMARY KEY,
   personalID    VARCHAR(11) UNIQUE NOT NULL,
-  username      VARCHAR(50) UNIQUE NOT NULL,
   password      TEXT               NOT NULL,
   email         VARCHAR(50) UNIQUE NOT NULL,
   companyID     INT,
   firstName     TEXT               NOT NULL,
   lastName      TEXT               NOT NULL,
+  gender        ENUM('MALE', 'FEMALE') NOT NULL,
   phoneNumber   VARCHAR(13) UNIQUE NOT NULL,
   carID         VARCHAR(8)         NOT NULL,
   facebookID    TEXT, -- TODO trigger davadot uniquesi
   googleID      TEXT, -- TODO trigger davadot uniquesi
   rating        DOUBLE             NOT NULL,
-  preferencesID INT                NOT NULL,
+  DriverPreferenceID INT                NOT NULL,
   latitude      DECIMAL            NOT NULL,
   longitude     DECIMAL            NOT NULL,
   isActive      BOOLEAN            NOT NULL,
   FOREIGN KEY (carID) REFERENCES Cars (carID),
-  FOREIGN KEY (preferencesID) REFERENCES DriverPreferenes (ID),
+  FOREIGN KEY (DriverPreferenceID) REFERENCES DriverPreferences (DriverPreferenceID),
   FOREIGN KEY (companyID) REFERENCES Companies (companyID)
 );
 CREATE TABLE UserPreferences (
@@ -67,11 +66,11 @@ CREATE TABLE Users (
   firstName     TEXT                   NOT NULL,
   lastName      TEXT                   NOT NULL,
   phoneNumber   VARCHAR(13) UNIQUE     NOT NULL,
-  gender        ENUM("MALE", "FEMALE") NOT NULL,
+  gender        ENUM('MALE', 'FEMALE') NOT NULL,
   facebookID    TEXT,
   googleID      TEXT,
-  preferencesID INT                    NOT NULL,
-  FOREIGN KEY (preferencesID) REFERENCES UserPreferences (ID)
+  preferenceID INT                    NOT NULL,
+  FOREIGN KEY (preferenceID) REFERENCES UserPreferences (ID)
 );
 CREATE TABLE Orders (
   orderID            INT AUTO_INCREMENT PRIMARY KEY,
@@ -99,9 +98,3 @@ CREATE TABLE Reviews (
   FOREIGN KEY (userID) REFERENCES Users (userID),
   FOREIGN KEY (driverID) REFERENCES Drivers (driverID)
 );
-
-CREATE TABLE Queue (
-  queueID  INT AUTO_INCREMENT PRIMARY KEY,
-  userID   INT NOT NULL,
-  driverID INT NOT NULL
-)
