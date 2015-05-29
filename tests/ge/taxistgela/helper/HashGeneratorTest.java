e5329558b30fef0f89689528c5fcd850442bd2a4
Alex <alex.r.azizian@gmail.com>
2015-05-29 16:05:34 +0400

    Updated HashGeneratorTest.

--- tests/ge/taxistgela/helper/HashGeneratorTest.java
+++ tests/ge/taxistgela/helper/HashGeneratorTest.java
@@ -14,7 +14,12 @@ public class HashGeneratorTest {
     @Test
     public void testGetHash() throws Exception {
         String s = "HelloWorld";
-
         assertEquals("68e109f0f40ca72a15e05cc22786f8e6", HashGenerator.getHash(s));
+
+        s = "qwertyhgfdsazxcvbn";
+        assertEquals("ef1cc40bd58c24d5fc16fd2d74cdb8a1", HashGenerator.getHash(s));
+
+        s = "68e109f0f40ca72a15e05cc22786f8e6";
+        assertEquals("22744961b84dc9c20cdd1c0e0f5e1a82", HashGenerator.getHash(s));
     }
 }
