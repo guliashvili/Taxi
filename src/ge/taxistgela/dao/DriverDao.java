1a841122c8e84094d7a6d2fa8f6ac2384de2807c
Rati <rmach13@freeuni.edu.ge>
2015-05-29 14:32:59 +0300

    DriverDao Updated CompanyDao Updated

--- src/ge/taxistgela/dao/DriverDao.java
+++ src/ge/taxistgela/dao/DriverDao.java
@@ -1,8 +1,6 @@
 package ge.taxistgela.dao;
 
-import ge.taxistgela.bean.Driver;
-import ge.taxistgela.bean.Location;
-import ge.taxistgela.bean.UserPreferences;
+import ge.taxistgela.bean.*;
 import ge.taxistgela.db.DBConnectionProvider;
 
 import java.sql.Connection;
@@ -15,7 +13,8 @@ import java.util.List;
  * Created by Alex on 5/25/2015.
  */
 public class DriverDao implements DriverDaoAPI, OperationCodes {
-    final static String login_STMT = "SELECT * FROM Drivers WHERE email=? AND password=?";
+    final static String login_STMT = "SELECT * FROM Drivers INNER JOIN Cars ON Drivers.CarID=Cars.CarID INNER JOIN DriverPreferences ON " +
+            "DriverPreferences.DriverPreferenceID=Driver.DriverPreferenceID WHERE email=? AND password=?";
     final static String register_STMT = "INSERT INTO Drivers (personalID,password,email,companyID,firstName,lastName,gender,phoneNumber,carID,facebookID,googleID,rating,DriverPreferenceID,latitude,longitude,isActive)" +
             " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     final static String update_STMT = "UPDATE Drivers SET personalID=?,password=?,email=?,companyID=?,firstName=?,lastName=?,gender=?,phoneNumber=?,carID=?,facebookID=?,googleID=?,rating=?,DriverPreferenceID=?,latitude=?,longitude=?,isActive=?";
@@ -34,8 +33,10 @@ public class DriverDao implements DriverDaoAPI, OperationCodes {
     public Driver loginDriver(String email, String password) {
         try(Connection con = DBConnectionProvider.getConnection()) {
             try (PreparedStatement st = con.prepareStatement(login_STMT)) {
+                Car car = new Car();
                 st.setString(1,email);
                 st.setString(2,password);
+                System.out.println(st.toString());
                 ResultSet res = st.executeQuery();
                 Driver output=new Driver();
                 output.setDriverID(res.getInt("driverID"));
@@ -47,14 +48,22 @@ public class DriverDao implements DriverDaoAPI, OperationCodes {
                 output.setLastName(res.getString("lastName"));
 
                 output.setGender(Driver.Gender.valueOf(res.getString("gender")));
+                car.setCarID(res.getString("CarID"));
+                car.setCarDescription(res.getString("carDescrption"));
+                car.setConditioning(res.getBoolean("conditioning"));
+                car.setCarYear(res.getInt("carYear"));
+                car.setNumPassengers(res.getInt("numPassengers"));
 
-                //TODO Car
+                output.setCar(car);
 
                 output.setPhoneNumber(res.getString("phoneNumber"));
                 output.setFacebookID(res.getString("facebookID"));
                 output.setGoogleID(res.getString("googleID"));
                 output.setRating(res.getDouble("rating"));
-                //TODO driverPreference
+                DriverPreferences pref = new DriverPreferences();
+                pref.setCoefficientPer(res.getDouble("coefficientPer"));
+                pref.setMinimumUserRating(res.getDouble("minimumUserRating"));
+                output.setPreferences(pref);
                 output.setLocation(new Location(res.getBigDecimal("latitude"),res.getBigDecimal("longitude")));
                 output.setIsActive(res.getBoolean("isActive"));
             }
@@ -69,6 +78,7 @@ public class DriverDao implements DriverDaoAPI, OperationCodes {
         try(Connection con = DBConnectionProvider.getConnection()) {
             try (PreparedStatement st = con.prepareStatement(register_STMT)) {
 
+                System.out.println(st.toString());
             }
         }catch (SQLException e){
 
@@ -81,6 +91,7 @@ public class DriverDao implements DriverDaoAPI, OperationCodes {
         try(Connection con = DBConnectionProvider.getConnection()) {
             try (PreparedStatement st = con.prepareStatement(update_STMT)) {
 
+                System.out.println(st.toString());
             }
         }catch (SQLException e){
 
