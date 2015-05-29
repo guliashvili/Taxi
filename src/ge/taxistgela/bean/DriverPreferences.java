0d50f01fa8d8095ada10f6fe456ec014159f5b0a
gio <vinmesmiti@gmail.com>
2015-05-29 21:42:01 +0400

    minor

--- src/ge/taxistgela/bean/DriverPreferences.java
+++ src/ge/taxistgela/bean/DriverPreferences.java
@@ -4,16 +4,16 @@ package ge.taxistgela.bean;
  * Created by Alex on 5/25/2015.
  */
 public class DriverPreferences {
-    private int driverPreferencesID;
+    private int DriverPreferenceID;
     private double minimumUserRating;
     private double coefficientPer; //yovel wutze an mandzilze ra tarifi aqvs. amas mere shevxedevat riti gvirchevnia davtvalot
 
-    public int getDriverPreferencesID() {
-        return driverPreferencesID;
+    public int getDriverPreferenceID() {
+        return DriverPreferenceID;
     }
 
-    public void setDriverPreferencesID(int driverPreferencesID) {
-        this.driverPreferencesID = driverPreferencesID;
+    public void setDriverPreferenceID(int driverPreferenceID) {
+        DriverPreferenceID = driverPreferenceID;
     }
 
     public double getMinimumUserRating() {
