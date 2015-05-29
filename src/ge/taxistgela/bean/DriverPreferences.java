ffb31b400d223d663eb00150ed4d0d735be31546
gio <vinmesmiti@gmail.com>
2015-05-29 21:40:58 +0400

    driverPreferences class id aklda

--- src/ge/taxistgela/bean/DriverPreferences.java
+++ src/ge/taxistgela/bean/DriverPreferences.java
@@ -4,8 +4,17 @@ package ge.taxistgela.bean;
  * Created by Alex on 5/25/2015.
  */
 public class DriverPreferences {
+    private int driverPreferencesID;
     private double minimumUserRating;
     private double coefficientPer; //yovel wutze an mandzilze ra tarifi aqvs. amas mere shevxedevat riti gvirchevnia davtvalot
+
+    public int getDriverPreferencesID() {
+        return driverPreferencesID;
+    }
+
+    public void setDriverPreferencesID(int driverPreferencesID) {
+        this.driverPreferencesID = driverPreferencesID;
+    }
 
     public double getMinimumUserRating() {
         return minimumUserRating;
