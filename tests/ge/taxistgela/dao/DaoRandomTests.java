ea314dcf7335d6a96e286674230fb95dbc3609ef
Ratmach <rmach13@freeuni.edu.ge>
2015-06-06 15:29:28 +0400

    created randomUserTests which fills up database with alot of users with more or less valid names

--- tests/ge/taxistgela/dao/DaoRandomTests.java
+++ tests/ge/taxistgela/dao/DaoRandomTests.java
@@ -1,9 +1,6 @@
 package ge.taxistgela.dao;
 
-import ge.taxistgela.bean.Company;
-import ge.taxistgela.bean.Driver;
-import ge.taxistgela.bean.User;
-import ge.taxistgela.bean.UserPreference;
+import ge.taxistgela.bean.*;
 import ge.taxistgela.model.*;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -15,13 +12,9 @@ import java.util.Random;
  * Created by Ratmach on 6/6/15.
  */
 public class DaoRandomTests extends TestCase {
-    String[] names={};
-    String[] surenames={};
-    String[] emails={};
-    String[] passwords={};
-    String[] phoneNumbers={};
-    String[] facebook_IDS={};
-    String[] google_IDS={};
+    String[] names={"rati","azo","gela","gulo","mandzu","aleqsandre","tornike","giorgi","givi","nodari","sandro","ana","nino","nunu","ekko","yasuo","vi","volibear","tinker","revazi","vlad","roshan","seer"};
+    String[] surenames={"matchavariani","aziziani","guliashvili","mandzulashvili","magaltadze","magradze","kunchulia","gobejishvili","tchanturia","dogonadze","shalikashvili"};
+    String[] emails={"@freeuni.edu.ge","@gmail.com","@yahoo.com","@mail.ru"};
 
     String[] companyNames={};
 
@@ -32,9 +25,32 @@ public class DaoRandomTests extends TestCase {
     UserPreference[] preferences={};
     @Test
     public void randomUserTests(){
-        UserManager man = new UserManager(new UserDao());
+        UserDao dao = new UserDao();
+        Random rnd = new Random();
         for(int i=0;i<250;++i){
-
+            String email = "";
+            while(email.equals("") || dao.checkEmail(email)){
+                email = generateRandomString(14,false,false)+emails[rnd.nextInt(emails.length)];
+            }
+            String password = generateRandomString(10,true,false);
+            String name = names[rnd.nextInt(names.length)];
+            String surename = surenames[rnd.nextInt(surenames.length)];
+            String phoneNumber = generateRandomString(9,true,true);
+            Gender gend=Gender.MALE;
+            if(rnd.nextBoolean()) gend=Gender.FEMALE;
+            String facebookID = generateRandomString(30,true,false);
+            String googleID = generateRandomString(30,true,false);
+            double rating = rnd.nextDouble()*5;
+            UserPreference usrp = new UserPreference();
+            usrp.setUserPreferenceID(-1);
+            usrp.setCarYear(rnd.nextInt(25)+1990);
+            usrp.setConditioning(rnd.nextBoolean());
+            usrp.setMinimumDriverRating(rnd.nextDouble()*5);
+            usrp.setTimeLimit(rnd.nextInt(2500));
+            usrp.setWantsAlone(rnd.nextBoolean());
+            dao.insertUserPreference(usrp);
+            User usr = new User(-1,email,password,name,surename,phoneNumber,gend,facebookID,googleID,rating,usrp,rnd.nextBoolean());
+            dao.registerUser(usr);
         }
     }
     @Test
@@ -50,13 +66,19 @@ public class DaoRandomTests extends TestCase {
      * @param forceLength the output string should be exactly given size
      * @return
      */
-    private String generateRandomString(int len,boolean forceLength){
+    private String generateRandomString(int len,boolean forceLength,boolean numbersOnly){
         Random rnd = new Random();
         String output="";
         for(int i=0;i<len;++i){
-            if(rnd.nextBoolean() || forceLength){
-                output+='a'+rnd.nextInt(26);
-            }
+            if(rnd.nextBoolean() || numbersOnly)
+                output += '0' + rnd.nextInt(9);
+            else
+                if(rnd.nextBoolean() || forceLength)
+                    if(rnd.nextBoolean())
+                        output += 'a' + rnd.nextInt(26);
+                    else
+                        output += '0' + rnd.nextInt(9);
+
         }
         return output;
     }
