b94b28d5a980637140e0c01da70d34cff86dde86
ratmach <ratmach@yahoo.com>
2015-06-07 05:47:49 +0400

    Added Clean File For Manager Tests

---
+++ tests/ge/taxistgela/model/ManagerTests.java
@@ -0,0 +1,48 @@
+package ge.taxistgela.model;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Ratmach on 7/6/15.
+ */
+public class ManagerTests extends TestCase {
+    @Before
+    public void setUp(){
+
+    }
+    @Test
+    public void companyManagerTests(){
+
+    }
+    @Test
+    public void driverManagerTests(){
+
+    }
+    @Test
+    public void orderManagerTests(){
+
+    }
+    @Test
+    public void reviewManagerTests(){
+
+    }
+    @Test
+    public void SessionManagerTests(){
+
+    }
+    @Test
+    public void taxUserQueueTests(){
+
+    }
+    @Test
+    public void userManagerTests(){
+
+    }
+    @After
+    public void cleanUp(){
+
+    }
+}
