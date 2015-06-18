8b64374a0dc124e73206c53e64d0e5b3bc03c487
Alex <alex.r.azizian@gmail.com>
2015-06-18 14:09:15 +0400

    Updated ContextListener.

--- src/ge/taxistgela/listener/ContextListener.java
+++ src/ge/taxistgela/listener/ContextListener.java
@@ -2,10 +2,10 @@ package ge.taxistgela.listener; /**
  * Created by Alex on 6/5/2015.
  */
 
+import ge.taxistgela.dao.*;
 import ge.taxistgela.dispatcher.OrderDispatcher;
 import ge.taxistgela.helper.ExternalAlgorithms;
-import ge.taxistgela.model.SessionManager;
-import ge.taxistgela.model.SessionManagerAPI;
+import ge.taxistgela.model.*;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -33,12 +33,32 @@ public class ContextListener implements ServletContextListener,
          You can initialize servlet context related data here.
       */
         ServletContext sc = sce.getServletContext();
+
+        // add CompanyManager.
+        sc.setAttribute(CompanyManagerAPI.class.getName(), new CompanyManager(new CompanyDao()));
+
+        // add DriverManager.
+        sc.setAttribute(DriverManagerAPI.class.getName(), new DriverManager(new DriverDao()));
+
+        // add OrderManager.
+        sc.setAttribute(OrderManagerAPI.class.getName(), new OrderManager(new OrderDao()));
+
+        // add ReviewManager.
+        sc.setAttribute(ReviewManagerAPI.class.getName(), new ReviewManager(new ReviewDao()));
+
+        // add SessionManager.
         sc.setAttribute(SessionManagerAPI.class.getName(), new SessionManager());
 
+        // add UserManager.
+        sc.setAttribute(UserManagerAPI.class.getName(), new UserManager(new UserDao()));
+
+        // add OrderDispatcher.
         OrderDispatcher orderDispatcher = new OrderDispatcher(sc);
         sc.setAttribute(OrderDispatcher.class.getName(), orderDispatcher);
         ExternalAlgorithms.debugPrint("Starting order dispatcher...");
         orderDispatcher.start();
+
+
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
@@ -46,8 +66,27 @@ public class ContextListener implements ServletContextListener,
          (the Web application) is undeployed or 
          Application Server shuts down.
       */
-        sce.getServletContext().removeAttribute(SessionManagerAPI.class.getName());
+        ServletContext sc = sce.getServletContext();
 
+        // remove CompanyManager.
+        sc.removeAttribute(CompanyManagerAPI.class.getName());
+
+        // remove DriverManager.
+        sc.removeAttribute(DriverManagerAPI.class.getName());
+
+        // remove OrderManager.
+        sc.removeAttribute(OrderManagerAPI.class.getName());
+
+        // remove ReviewManager.
+        sc.removeAttribute(ReviewManagerAPI.class.getName());
+
+        // remove SessionManager.
+        sc.removeAttribute(SessionManagerAPI.class.getName());
+
+        // remove UserManager.
+        sc.removeAttribute(UserManagerAPI.class.getName());
+
+        // remove OrderDispatcher.
         OrderDispatcher orderDispatcher = (OrderDispatcher) sce.getServletContext().getAttribute(OrderDispatcher.class.getName());
         orderDispatcher.cancel();
         sce.getServletContext().removeAttribute(OrderDispatcher.class.getName());
