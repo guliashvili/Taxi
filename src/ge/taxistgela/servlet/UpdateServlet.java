74dc69ecd675de82fdf82a013c0776c186073657
Alex <alex.r.azizian@gmail.com>
2015-06-26 01:36:13 +0400

    Added stub servlets.

---
+++ src/ge/taxistgela/servlet/UpdateServlet.java
@@ -0,0 +1,36 @@
+package ge.taxistgela.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Alex on 6/26/2015.
+ */
+@WebServlet("/update")
+public class UpdateServlet extends ActionServlet {
+
+    public void uPreferences(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void dPreferences(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void uPassword(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void dPassword(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void cPassword(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void dStatus(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
