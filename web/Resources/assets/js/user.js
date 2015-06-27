05f2087b32b9497c4e07c2f2a8ef5f0e38000734
ratmach <ratmach@yahoo.com>
2015-06-28 01:00:19 +0400

    fixed order adding form

--- web/Resources/assets/js/user.js
+++ web/Resources/assets/js/user.js
@@ -4,11 +4,54 @@
 $(document).ready(function(){
     initializeO();
 });
+var records;
 function initializeO(){
     initializeMap(true);
     createPreferencesSaves();
+    $.ajax({
+        url: "/order",
+        method: "post",
+        data: {action:"getUserOrders"},
+        cache: false,
+        success: function (data) {
+            records=JSON.parse(data);
+            generateGrid();
+        },
+        error: function (data) {
+            console.error("Couldn't log in\n" + JSON.stringify(formData));
+        }
+    });
 }
-
+function generateGrid(){
+    $('#grid').w2grid({
+        name: 'grid',
+        header: 'List of Names',
+        show: {
+            toolbar: true,
+            footer: true
+        },
+        columns: [
+            { field: 'orderID', caption: 'orderID', size: '50px', sortable: true, attr: 'align=center' },
+            { field: 'userID', caption: 'userID', size: '30%', sortable: true, resizable: false },
+            { field: 'driverID', caption: 'driverID', size: '30%', sortable: true, resizable: false },
+            { field: 'numPassengers', caption: 'numPassengers', size: '40%', resizable: true },
+            { field: 'startLocation', caption: 'startLocation', size: '120px', resizable: true },
+            { field: 'endLocation', caption: 'endLocation', size: '120px', resizable: true },
+            { field: 'startTime', caption: 'startTime', size: '120px', resizable: true },
+            { field: 'endTime', caption: 'endTime', size: '120px', resizable: true },
+            { field: 'paymentAmount', caption: 'paymentAmount', size: '120px', resizable: true },
+            { field: 'callTime', caption: 'callTime', size: '120px', resizable: true },
+            { field: 'revokedByUser', caption: 'revokedByUser', size: '120px', resizable: true },
+            { field: 'revokedByDriver', caption: 'revokedByDriver', size: '120px', resizable: true },
+        ],
+        searches: [
+            { field: 'driverID', caption: 'driverID', type: 'text' },
+            { field: 'orderID', caption: 'orderID', type: 'text' },
+        ],
+        sortData: [{ field: 'orderID', direction: 'ASC' }],
+        records: records
+    });
+}
 function askForDate(){
     if(endMarker!=null){
         askWindow.open(map,startMarker);
