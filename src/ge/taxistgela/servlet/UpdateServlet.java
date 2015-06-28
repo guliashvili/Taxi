1cf3a4340feb13f38908b13eceb9d0f664ed4278
Alex <alex.r.azizian@gmail.com>
2015-06-28 17:50:28 +0400

    Updated UpdateServlet, SocialNetworkServlet, added update to session user.

--- src/ge/taxistgela/servlet/UpdateServlet.java
+++ src/ge/taxistgela/servlet/UpdateServlet.java
@@ -46,6 +46,9 @@ public class UpdateServlet extends ActionServlet {
                     userPreference.setWantsAlone("on".equals(request.getParameter("wantsAlone")));
                 } catch (Exception ex) {
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+                    updateSessionUser(User.class.getName(), userManager, request, user);
+
                     return;
                 }
 
@@ -54,7 +57,8 @@ public class UpdateServlet extends ActionServlet {
                 if (errorCode.errorNotAccrued()) {
                     response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
-                    user.setPreference(userPreference);
+                    updateSessionUser(User.class.getName(), userManager, request, user);
+
                     return;
                 }
             }
@@ -64,6 +68,8 @@ public class UpdateServlet extends ActionServlet {
             if (errorCode != null) {
                 response.getWriter().print(errorCode.toJson());
             }
+
+            updateSessionUser(User.class.getName(), userManager, request, user);
         }
     }
 
@@ -86,6 +92,8 @@ public class UpdateServlet extends ActionServlet {
                 } catch (Exception ex) {
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 
+                    updateSessionUser(Driver.class.getName(), driverManager, request, driver);
+
                     return;
                 }
 
@@ -94,7 +102,8 @@ public class UpdateServlet extends ActionServlet {
                 if (errorCode.errorNotAccrued()) {
                     response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
-                    driver.setPreferences(driverPreference);
+                    updateSessionUser(Driver.class.getName(), driverManager, request, driver);
+
                     return;
                 }
             }
@@ -104,6 +113,8 @@ public class UpdateServlet extends ActionServlet {
             if (errorCode != null) {
                 response.getWriter().print(errorCode.toJson());
             }
+
+            updateSessionUser(Driver.class.getName(), driverManager, request, driver);
         }
     }
 
@@ -144,6 +155,7 @@ public class UpdateServlet extends ActionServlet {
                 if (errorCode.errorNotAccrued()) {
                     response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
+                    updateSessionUser(A_TYPE[type], man, request, obj);
                     return;
                 }
             }
@@ -153,6 +165,8 @@ public class UpdateServlet extends ActionServlet {
             if (errorCode != null) {
                 response.getWriter().print(errorCode.toJson());
             }
+
+            updateSessionUser(A_TYPE[type], man, request, obj);
         }
     }
 
@@ -202,6 +216,8 @@ public class UpdateServlet extends ActionServlet {
                 if (errorCode.errorNotAccrued()) {
                     response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
+                    updateSessionUser(Driver.class.getName(), driverManager, request, driver);
+
                     return;
                 }
             }
@@ -211,6 +227,8 @@ public class UpdateServlet extends ActionServlet {
             if (errorCode != null) {
                 response.getWriter().print(errorCode.toJson());
             }
+
+            updateSessionUser(Driver.class.getName(), driverManager, request, driver);
         }
     }
 
@@ -234,16 +252,17 @@ public class UpdateServlet extends ActionServlet {
                     car.setNumPassengers(Integer.parseInt(request.getParameter("numPassengers")));
                 } catch (Exception ex) {
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+                    updateSessionUser(Driver.class.getName(), driverManager, request, driver);
                     return;
                 }
 
                 errorCode = driverManager.updateCar(car);
 
                 if (errorCode.errorNotAccrued()) {
-                    driver.setCar(car);
-
                     response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
+                    updateSessionUser(Driver.class.getName(), driverManager, request, driver);
                     return;
                 }
             }
@@ -253,6 +272,13 @@ public class UpdateServlet extends ActionServlet {
             if (errorCode != null) {
                 response.getWriter().print(errorCode.toJson());
             }
+
+            updateSessionUser(Driver.class.getName(), driverManager, request, driver);
         }
+    }
+
+    private void updateSessionUser(String aType, SuperUserManager um, HttpServletRequest request, SuperDaoUser superUser) {
+        superUser = um.getByEmail(superUser.getEmail());
+        request.getSession().setAttribute(aType, superUser);
     }
 }
