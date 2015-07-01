e18f94c7aaed12ed946f3fddaa2b9d982a1ecd8f
gio <vinmesmiti@gmail.com>
2015-07-01 06:36:48 +0400

    early bird last song

--- src/ge/taxistgela/ram/bean/Route.java
+++ src/ge/taxistgela/ram/bean/Route.java
@@ -28,11 +28,13 @@ public class Route implements Serializable {
         this.users = users;
     }
 
-    public static double getDistance(List<RouteElement> tmp) {
+    public static double getDistance(Location start, List<RouteElement> tmp) {
         double ret = 0;
         for (int i = 1; i < tmp.size(); i++) {
             ret += GoogleMapUtils.getRoad(tmp.get(i - 1).getLoc(), tmp.get(i).getLoc()).distance.inMeters;
         }
+        if (tmp.size() > 0) ret += GoogleMapUtils.getRoad(start, tmp.get(0).getLoc()).distance.inMeters;
+
         ret /= 1000;
 
         return ret;
@@ -72,7 +74,7 @@ public class Route implements Serializable {
             RouteElement elem = tmp.get(i);
             tmp.remove(i);
 
-            bestAns = findTheBest(last, nextCurTime,
+            bestAns = findTheBest(elem.getLoc(), nextCurTime,
                     tmp, bestAns, cur);
 
             tmp.add(i, elem);
@@ -95,21 +97,25 @@ public class Route implements Serializable {
     }
 
     public double getOptimalPriceIfInserted(OrderInfo orderInfo) {
+
+
         List<RouteElement> tmp = new ArrayList<>();
         tmp.addAll(route);
 
-        double dist0 = getDistance(tmp);
+        double dist0 = getDistance(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(), tmp);
 
-        tmp.add(new RouteElement(orderInfo.getStart(), orderInfo, false));
-        tmp.add(new RouteElement(orderInfo.getEnd(), orderInfo, true));
+        List<RouteElement> tmp2 = new ArrayList<>();
+        tmp2.add(new RouteElement(orderInfo.getStart(), orderInfo, true));
+        tmp2.add(new RouteElement(orderInfo.getEnd(), orderInfo, false));
 
-        tmp.sort((o1, o2) -> o1.getOrderInfo().getOrderID() - o2.getOrderInfo().getOrderID());
+        double M = getDistance(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(), tmp2);
 
+        tmp.addAll(tmp2);
 
-        double dist1 = findTheBest(drivers.get(tmp.get(0).getOrderInfo().getDriver().getDriverID()).getLocation(),
+        double dist1 = findTheBest(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(),
                 TimeUnit.MILLISECONDS.toMinutes(new Date().getTime()), tmp, 99999999, 0);
 
-        double M = orderInfo.getDistance();
+
 
         int n = getCount(orderInfo.getOrderID());
         double me = dist1 - dist0;
@@ -117,24 +123,108 @@ public class Route implements Serializable {
 
         payD *= orderInfo.getDriver().getPreferences().getCoefficientPer();
 
-        payD = Math.min(payD, orderInfo.getMaxPrice());
+        payD = Math.max(payD, 1.0);
 
         return payD;
     }
 
-    public synchronized void updateLocations() {
+    public synchronized double updateLocations(Location last, double curTime, List<RouteElement> tmp, double bestAns, double curAns,
+                                               List<RouteElement> current, List<RouteElement> best) {
+        if (bestAns < curAns)
+            return bestAns;
 
+        if (tmp.size() == 0) {
+            best.clear();
+            best.addAll(current);
+
+            return curAns;
+        }
+
+
+        for (int i = 0; i < tmp.size(); i++) {
+            boolean isNotLogical = false;
+            double nextCurTime = curTime + GoogleMapUtils.getRoad(last, tmp.get(i).getLoc()).duration.inSeconds / 60.0;
+
+            if (!tmp.get(i).isPickUser()) {
+                for (RouteElement elem : tmp) {
+                    if (elem.isPickUser() &&
+                            elem.getOrderInfo().getOrderID() == tmp.get(i).getOrderInfo().getOrderID()) {
+                        isNotLogical = true;
+                        break;
+                    }
+                }
+
+                isNotLogical |= (nextCurTime - tmp.get(i).getOrderInfo().getCreateTime()) >
+                        tmp.get(i).getOrderInfo().getUser().getPreference().getTimeLimit();
+            }
+
+
+            if (isNotLogical) continue;
+
+            double cur = curAns;
+
+            cur += GoogleMapUtils.getRoad(last, tmp.get(i).getLoc()).distance.inMeters / 1000.0;
+
+
+            RouteElement elem = tmp.get(i);
+            current.add(elem);
+            tmp.remove(i);
+
+            bestAns = updateLocations(elem.getLoc(), nextCurTime,
+                    tmp, bestAns, cur, current, best);
+
+            current.remove(current.size() - 1);
+            tmp.add(i, elem);
+
+        }
+        return bestAns;
     }
 
     public synchronized void addOrder(OrderInfo orderInfo) {
-        RouteElement a = new RouteElement(orderInfo.getStart(), orderInfo, true);
-        RouteElement b = new RouteElement(orderInfo.getEnd(), orderInfo, false);
+        List<RouteElement> tmp = new ArrayList<>();
+        tmp.addAll(route);
 
-        route.add(a);
-        route.add(b);
-        if (route.size() > 2)
-            updateLocations();
+        double dist0 = getDistance(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(), tmp);
 
+        List<RouteElement> tmp2 = new ArrayList<>();
+        tmp2.add(new RouteElement(orderInfo.getStart(), orderInfo, true));
+        tmp2.add(new RouteElement(orderInfo.getEnd(), orderInfo, false));
+
+        double M = getDistance(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(), tmp2);
+
+        tmp.addAll(tmp2);
+
+        List<RouteElement> current = new ArrayList<>();
+        List<RouteElement> best = new ArrayList<>();
+        double dist1 = updateLocations(drivers.get(orderInfo.getDriver().getDriverID()).getLocation(),
+                TimeUnit.MILLISECONDS.toMinutes(new Date().getTime()), tmp, 99999999, 0, current, best);
+
+
+        int n = getCount(orderInfo.getOrderID());
+        double me = dist1 - dist0;
+        double payMe = me + (M - me) / (n + 1);
+        double addOther = 0;
+        if (n > 0) {
+            addOther = (M - me) / (n + 1) - (M - me) / n;
+        }
+        addOther *= orderInfo.getDriver().getPreferences().getCoefficientPer();
+        payMe *= orderInfo.getDriver().getPreferences().getCoefficientPer();
+
+        for (RouteElement elem : best) {
+            if (elem.getOrderInfo().getUser().getUserID() == orderInfo.getOrderID()) {
+                payMe = Math.max(payMe, 1.0);
+                payMe = Math.min(payMe, elem.getOrderInfo().getMaxPrice());
+                elem.getOrderInfo().setMaxPrice(payMe);
+            } else {
+                double price = Math.max(elem.getOrderInfo().getMaxPrice() + addOther, 1);
+                price = Math.min(price, elem.getOrderInfo().getMaxPrice());
+                elem.getOrderInfo().setMaxPrice(price);
+            }
+
+        }
+
+        route = best;
+        
     }
 
     public synchronized void pickUser(UserInfo userInfo, int orderID) {
